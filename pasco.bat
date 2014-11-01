8a9db8051dc0703583d8789c85ca9c6636262958
Mateo <mxc5617@rit.edu>
2014-11-01 16:19:35 -0400

    Created pasco.db
    This file creates a db file of IE9's (and below) browsing history. That db file will then be put into a database for manipulation on a map.

---
+++ pasco.bat
@@ -0,0 +1,9 @@
+#this is for IE9
+#Batch file needs to be in C:\Users\"Username"
+#pasco must be installed on C:\Users\"Username"
+#this script will spit out indexdata.db on the root of C:\
+#The db file will contain IE history nicely formatted
+
+#C:\Downloads\pasco\bin\pasco.exe \AppData\local\Microsoft\Windows\History\History.IE\index.dat > C:\indexdata.db
+
+C:\Users\Default\pasco\pasco\bin\pasco.exe \AppData\local\Microsoft\Windows\History\History.IE\index.dat > C:\indexdata.db
